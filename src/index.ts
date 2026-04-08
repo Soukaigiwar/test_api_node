c81a4c3f4ced056ca61792984c3f7ec1d5083282
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2026-04-08 10:18:53 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/health', (req: Request, res: Response) => {
-    res.status(200).json({status: 'API rodando com sucesso!'});
+    res.status(200).json({status: 'API rodando com sucesso versao atualizada!'});
 });
 
 app.listen(3000, () => {
